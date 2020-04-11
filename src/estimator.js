d4b2af4b4397b7d916424c9d691b107fc4c74026
Victor-El <victorelezua@gmail.com>
2020-04-11 16:49:02 +0100

    complete challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,41 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable no-restricted-properties */
+const covid19ImpactEstimator = (data) => {
+  impact = {};
+  severeImpact = {};
+
+  impact.currentlyInfected = computeImpactCurrentlyInfected(data.reportedCases);
+  impact.infectionsByRequestedTime = computeInfectionsByRequestedTime(impact.currentlyInfected);
+
+  severeImpact.currentlyInfected = computeSevereImpactCurrentlyInfected(data.reportedCases);
+  severeImpact.infectionsByRequestedTime = computeInfectionsByRequestedTime(severeImpact.currentlyInfected);
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
+
+const computeImpactCurrentlyInfected = (reportedCases) => reportedCases * 10;
+const computeSevereImpactCurrentlyInfected = (reportedCases) => reportedCases * 50;
+const computeInfectionsByRequestedTime = (currentlyInfected, requestedTime, periodType) => {
+  let infectionsByRequestedTime = 0;
+  switch (periodType.toLowerCase()) {
+    case 'days':
+      infectionsByRequestedTime = currentlyInfected * (Math.pow(2, Math.floor(requestedTime / 3)));
+      break;
+    case 'weeks':
+      infectionsByRequestedTime = currentlyInfected * (Math.pow(2, Math.floor((requestedTime * 7) / 3)));
+      break;
+    case 'months':
+      infectionsByRequestedTime = currentlyInfected * (Math.pow(2, Math.floor((requestedTime * 30) / 3)));
+      break;
+    default:
+      throw new Error('Invalid argument, periodType must be either days, weeks or months');
+  }
+
+  return infectionsByRequestedTime;
+};
+
 
 export default covid19ImpactEstimator;
